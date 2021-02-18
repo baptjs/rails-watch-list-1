fc44465ab712c6d879ea926fb0abf9cefadbb5b0
Lucien George <luciengeorge95@gmail.com>
2021-02-18 18:01:29 +0000

    routes added for saved_movies#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
-  resources :lists, except: [ :edit, :update ] do
-    resources :saved_movies, only: :create
+  resources :lists, except: [:edit, :update] do
+    resources :saved_movies, only: [:new, :create]
   end
   resources :saved_movies, only: :destroy
 end
