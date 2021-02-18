15330e64e8fcbdfaef7c1f78eba5d278be09ee12
Anne de Joly <anne.dejoly@edhec.com>
2021-02-18 16:50:10 +0000

    Added saved movie and list tests

--- app/models/saved_movie.rb
+++ app/models/saved_movie.rb
@@ -2,5 +2,6 @@ class SavedMovie < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
+  validates :comment, length: { minimum: 6 }
   validates :movie_id, uniqueness: { scope: :list_id, message: "This movie has already been saved!" }
 end
