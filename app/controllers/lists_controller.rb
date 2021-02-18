38cee50a1dd448c7190c92531ea960dd48d62996
Lucien George <luciengeorge95@gmail.com>
2021-02-18 16:55:43 +0000

    wip view and controllers

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,36 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: [ :show, :destroy ]
+  def index
+    @lists = List.all
+  end
+
+  def show; end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect to lists_path
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
--- app/controllers/saved_movies_controller.rb
+++ app/controllers/saved_movies_controller.rb
@@ -1,2 +1,29 @@
 class SavedMoviesController < ApplicationController
+  before_action :set_saved_movie, only: :destroy
+
+  def create
+    @saved_movie = SavedMovie.new(saved_movie_params)
+    @list = List.find(params[:list_id])
+    @saved_movie.list = @list
+    if @saved_movie.save
+      redirect_to list_path(@list)
+    else
+      render 'lists/show'
+    end
+  end
+
+  def destroy
+    @saved_movie.destroy
+    redirect_to list_path(@saved_movie.list)
+  end
+
+  private
+
+  def saved_movie_params
+    params.require(:comment, :movie_id)
+  end
+
+  def set_saved_movie
+    @saved_movie = SavedMovie.find(params[:id])
+  end
 end
