d96a5035b502eb62b2111269be4ed01fafbf1f55
Lucien George <luciengeorge95@gmail.com>
2021-02-19 16:07:09 +0000

    wip reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,18 @@
+class ReviewsController < ApplicationController
+  def create
+    @review = Review.new(review_params)
+    @list = List.find(params[:list_id])
+    @review.list = @list
+    if @review.save
+      redirect_to list_path(@list)
+    else
+      render 'lists/show'
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to list_path(@review.list)
+  end
+end
